b483ffe17a4e3c8e7cc1f12c0a3dff4b0f7ca276
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2024-05-11 14:11:28 +0100

    ztech

--- lib/widgets/main_mobile.dart
+++ lib/widgets/main_mobile.dart
@@ -66,10 +66,6 @@ class MainMobile extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              MobileProfileAnimation(),
-              const SizedBox(
-                height: 20,
-              ),
               Row(
                 children: [
                   FadeInLeft(
@@ -172,10 +168,10 @@ class MainMobile extends StatelessWidget {
                     stopPauseOnTap: true,
                     repeatForever: true),
               ),
-              // const SizedBox(
-              //   height: 20,
-              // ),
-              // MobileProfileAnimation()
+              const SizedBox(
+                height: 20,
+              ),
+              MobileProfileAnimation()
             ],
           ),
           // Padding(
