0ff29cd85b91558a0a8a168fbd91598235f2f2ef
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2024-05-13 12:22:59 +0100

    ztech

--- lib/widgets/main_mobile.dart
+++ lib/widgets/main_mobile.dart
@@ -16,7 +16,7 @@ class MainMobile extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
     final screenWidth = screenSize.width;
     final screenHeight = screenSize.height;
-    double multiplier = 0.06;
+    double multiplier = 0.1;
 
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 30),
